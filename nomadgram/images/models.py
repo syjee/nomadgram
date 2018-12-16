f0cb0fabed9e07c9b74a3ff9e52b7b08bb68a924
jisoyeong <syjee95@kpu.ac.kr>
2018-12-17 02:25:47 +0900

    created images model

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #abstract base Model
+    #This model will not be used to create any database table.
+    class Meta:
+        abstract = True
+
+
+class Images(TimeStampedModel):
+    
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+
+class Comment(TimeStampedModel):
+
+    message = models.TextField()
