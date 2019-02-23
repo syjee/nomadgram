af7d5ece8f840a48116c9804e25af5371bcd3636
jisoyeong <syjee95@kpu.ac.kr>
2019-02-24 01:11:07 +0900

    moderate comments

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -11,19 +11,19 @@ urlpatterns = [
     ),
     url(
         #patterns, regular expressions, regex
-        regex = r'^(?P<image_id>[0-9]+)/like/',
+        regex = r'^(?P<image_id>[0-9]+)/like/$',
         view = views.LikeOnImage.as_view(),
         name = 'like_image'
     ),
     url(
         #patterns, regular expressions, regex
-        regex = r'^(?P<image_id>[0-9]+)/unlike/',
+        regex = r'^(?P<image_id>[0-9]+)/unlike/$',
         view = views.UnLikeOnImage.as_view(),
         name = 'like_image'
     ),
     
     url(
-        regex = r'^(?P<image_id>[0-9]+)/comments/' ,
+        regex = r'^(?P<image_id>[0-9]+)/comments/$' ,
         view = views.CommentOnImage.as_view(),
         name = 'comment_image'
     ),
@@ -33,9 +33,13 @@ urlpatterns = [
         name = 'delete_comment'
     ),
     url(
+        regex = r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$',
+        view = views.DeleteCommentOnMine.as_view(),
+        name = 'delete_comment_on_mine'
+    ),
+    url(
         regex = r'^search/$',
         view = views.Search.as_view(),
         name = "search_images"
     )
-
 ]
